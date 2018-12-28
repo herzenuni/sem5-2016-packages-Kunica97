b10893fe4130cdf5bd668c1bbedde0c90bf8f7a4
Sofiya Kunitsyna <31761683+Kunica97@users.noreply.github.com>
2018-12-28 16:28:22 +0300

    Add files via upload

---
+++ СР/ИНВ1.2/package/guest_test.py
@@ -0,0 +1,35 @@
+import unittest
+import guestBook
+import os
+
+
+class TestCase(unittest.TestCase):
+
+    def test_guest_book_file_exists(self):
+        self.assertTrue(os.path.isfile("./book.json"))
+
+    def test_guest_book_type(self):
+        self.assertTrue(isinstance(guestBook.GuestBook(), guestBook.GuestBook))
+
+    def test_guest_book_guests_type(self):
+        self.assertTrue(isinstance(guestBook.GuestBook().guests, list))
+
+    def test_guest_book_guests_add_remove(self):
+        book = guestBook.GuestBook()
+        book.add("Test1")
+        book.add("Test2")
+        book.add("Test3")
+        book.add("Test4")
+        book.remove("Test4")
+
+        lst = list([
+            {"Guest_name": "Test1"},
+            {"Guest_name": "Test2"},
+            {"Guest_name": "Test3"}
+        ])
+
+        self.assertEqual(book.guests, lst)
+
+
+if __name__ == "__main__":
+    unittest.main()
